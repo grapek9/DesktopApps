1f49cbd15ec994cc82e3002aee205a15340fc50f
grapek9 <zerex93@gmail.com>
2015-10-29 09:42:24 +0100

    minor changes

--- gui.py
+++ gui.py
@@ -1,18 +1,25 @@
 from PyQt4.QtGui import *
 from PyQt4.QtCore import *
+import sys
 from helpWindow import *
-
 class mainWindow(QWidget):
 	def __init__(self):
 		super(mainWindow,self).__init__()
 		self.initializeUI()
 	def initializeUI(self):
+		def test():
+			runHelpWindowGUI()
+			print('que1')
+		testButton = QPushButton('press me',self)
+		testButton.move(300,300)
+		testButton.clicked.connect(test)
 		self.setGeometry(200,200,640,480)
 		self.setWindowTitle('File Explorer By Grapek9')
 		self.show()
 def main():
 	application = QApplication(sys.argv)
-	start = mainWindow()
+	runGui = mainWindow()
 	sys.exit(application.exec_())
+
 if __name__ == "__main__":
-	main()
+    main()
--- helpWindow.py
+++ helpWindow.py
@@ -1,17 +1,19 @@
 from PyQt4.QtGui import *
 from PyQt4.QtCore import *
-
-class mainWindow(QWidget):
-	def __init__(self):
-		super(mainWindow,self).__init__()
-		self.initializeUI()
-	def initializeUI(self):
-		self.setGeometry(200,200,640,480)
-		self.setWindowTitle('File Explorer By Grapek9')
-		self.show()
-def main():
-	application = QApplication(sys.argv)
-	start = mainWindow()
-	sys.exit(application.exec_())
-if __name__ == "__main__":
-	main()
+import sys
+def runHelpWindowGUI():
+	class helpWindow(QWidget):
+		def __init__(self):
+			super(helpWindow,self).__init__()
+			self.initializeUI()
+		def initializeUI(self):
+			self.setGeometry(200,200,640,480)
+			self.setWindowTitle('Help Window')
+			self.show()
+	def main():
+		application = QApplication(sys.argv)
+		start = helpWindow()
+		sys.exit(application.exec_())
+	if __name__ == "__main__":
+		main()
+print("que2")
